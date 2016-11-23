d0efab77307fc1d0ff75096191f37122939ca848
XunGong <gongxun.cn@gmail.com>
2016-11-22 21:53:01 -0800

    Initial Commit

---
+++ VideoFrameProcessDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoFrameProcessDemo
+//
+//  Created by XunGong on 2016-11-22.
+//  Copyright © 2016 Xiomar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoFrameProcessDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoFrameProcessDemo
+//
+//  Created by XunGong on 2016-11-22.
+//  Copyright © 2016 Xiomar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
